456dc0a420be662bb18cebf1136a9978b656ee9d
abhiias262-lgtm <abhiias262@gmail.com>
2026-04-17 01:15:09 +0530

    Update traffic_fsm.v

--- traffic_fsm.v
+++ traffic_fsm.v
@@ -1,63 +1,61 @@
-module traffic_light (
-    input clk,
-    input rst,
-    output reg [2:0] signal 
+module traffic_light(
+    input clk,rst,
+    output reg [1:0] NT,WT,ST,ET
+
+
 );
-    parameter RED = 3'b100 ,
-            YELLOW= 3'b010 ,
-            GREEN = 3'b001;
-            
 
-    reg [1:0] state ,next_state;
-    reg[3:0] timer;
+    reg [1:0] current_state;
+    reg [1:0] next_state;
 
+// light signal 
+    parameter   RED = 01,
+                YELLOW=10,
+                GREEN=11,
+                NONE=00;
+
+// STATES 
+
+    parameter   IDLE=000,
+                NORTH=001,
+                SOUTH=010,
+                EAST=011,
+                WEST=100;
+
+
+
+// waiting time
+    parameter switching_time = 60 ;
+    reg [7:0] counter;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            state<=RED;
-            timer<=0;
-        end 
-        else begin
-            timer<=timer+1;
-            if (timer==9)begin
-                state<= next_state ;
-                timer<=0;
-            end
-        end        
+            counter<=8'b0;
+        end
+        else if(counter<=60) begin
+            counter<=counter+1'b1;
+        end
     end
+    wire timeout=(counter==switching_time);
 
-    always@(*)begin
-        case (state)
-            RED :    next_state=YELLOW;
-            YELLOW:  next_state=GREEN;
-            GREEN:   next_state=RED;
-            default: next_state=RED;
+    always @(posedge clk or posedge rst) begin
+    if(rst)begin
+        current_state<=IDLE;
+    end
+    else 
+        current_state<=next_state;
+    end
+    always @(*) begin 
+        next_state=current_state;
+        case (current_state)
+            IDLE:
+            NORTH:
+            EAST:
+            WEST:
+            SOUTH:
+      
+        
         endcase
     end
 
-    always @(*) begin
-        case (state)
-            RED:    signal=3'b100;
-            YELLOW: signal=3'b010;
-            GREEN:  signal=3'b001;
-            default:signal=3'b100;            
-        endcase
-    end
-endmodule
 
-module tb_traffic_light;
-    reg clk, rst;
-    wire [2:0] signal;
-
-    traffic_light dut (.clk(clk), .rst(rst), .signal(signal));
-
-    always #5 clk = ~clk;
-
-    initial begin
-        clk = 0; rst = 1;
-        #5 rst =0;
-        #200 ;
-        $finish;
-    end
-
-    initial $monitor("t=%0t signal=%b", $time, signal);
 endmodule
